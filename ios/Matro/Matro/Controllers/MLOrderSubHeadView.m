333fc997601e80bbe64d7275702917392e8210c9
黄裕华 <huangyuhua@huangyuhuadeMacBook-Air.local>
2016-07-21 16:03:11 +0800

    小修改

--- ios/Matro/Matro/Controllers/MLOrderSubHeadView.m
+++ ios/Matro/Matro/Controllers/MLOrderSubHeadView.m
@@ -37,8 +37,6 @@
 
 - (IBAction)saveClick:(id)sender {
     if (!_isFirst) { //第一次点击保存时
-        self.sfLeading.constant = 16;
-        self.fieldLeading.constant = 0;
         _isFirst = YES;
         [self saveShenFenzheng];
     }else{
@@ -56,8 +54,8 @@
 
 
 - (void)haveIdCardSave{
-    self.sfLeading.constant = 16;
-    self.fieldLeading.constant = 0;
+//    self.sfLeading.constant = 16;
+//    self.fieldLeading.constant = 0;
     _isFirst = YES;
     [self.editBtn setTitle:@"编辑" forState:UIControlStateNormal];
     self.shenfenzhengField.userInteractionEnabled = NO;
@@ -68,11 +66,9 @@
 
 - (void)saveShenFenzheng{
     if (self.shenfenzhengField.text.length > 0) { //
-        
-    }
     NSString *token = [[NSUserDefaults standardUserDefaults]objectForKey:kUSERDEFAULT_ACCCESSTOKEN];
     NSString *url = [NSString stringWithFormat:@"%@/api.php?m=member&s=admin_member&action=edit_identity_card&accessToken=%@",MATROJP_BASE_URL,[token URLEncodedString]];
-    NSDictionary *params = @{@"identity_card":self.shenfenzhengField.text,@"mobile":self.phoneLabel.text};
+    NSDictionary *params = @{@"identity_card":self.shenfenzhengField.text,@"mobile":self.phoneLabel.text,@"username":self.nameLabel.text?:@""};
     [MLHttpManager post:url params:params m:@"member" s:@"admin_member" success:^(id responseObject) {
         NSDictionary *result = (NSDictionary *)responseObject;
         if ([result[@"code"] isEqual:@0]) {
@@ -87,12 +83,11 @@
             if (self.orderSubChangeInfo) {
                 self.orderSubChangeInfo(msg);
             }
-            if (self.idcardisOk) {
-                self.idcardisOk(NO);
-            }
+            
         }
     } failure:^(NSError *error) {
     }];
+    }
 }
 
 - (void)setIsShowSFZ:(BOOL)isShowSFZ{
