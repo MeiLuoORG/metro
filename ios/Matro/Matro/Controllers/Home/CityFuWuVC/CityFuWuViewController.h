f9371402711bcd6496d066798376f3a0b83413a8
Zl335463762 <335463762@qq.com>
2016-07-22 17:37:03 +0800

    tibia

--- ios/Matro/Matro/Controllers/Home/CityFuWuVC/CityFuWuViewController.h
+++ ios/Matro/Matro/Controllers/Home/CityFuWuVC/CityFuWuViewController.h
@@ -13,7 +13,10 @@
 #import "HFSServiceClient.h"
 #import "ChongZhiViewController.h"
 #import "MLLoginViewController.h"
+#import "MLHttpManager.h"
+#import <SDWebImage/UIImageView+WebCache.h>
 
 @interface CityFuWuViewController : MLBaseViewController
+@property (weak, nonatomic) IBOutlet UIImageView *topImageView;
 
 @end
